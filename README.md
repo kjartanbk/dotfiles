9398d86bdf91f67f4904698a09c1299078bc0d66
Kjartan Kristjansson <kjartan.kristjansson@gmail.com>
2015-08-19 11:54:00 +0000

    Updated powerline installation instructions

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 To install, run `config.sh` in the directory `~/dotfiles`.
 
+For the vim powerline to work, run 
+`sudo apt-get install python-pip`
+and
+`pip install --user git+git://github.com/Lokaltog/powerline`
+
 
--- config.sh
+++ config.sh
@@ -5,3 +5,10 @@ ln -s /home/$USER/dotfiles/.vimrc /home/$USER/.vimrc
 ln -s /home/$USER/dotfiles/.bashrc /home/$USER/.bashrc
 ln -s /home/$USER/dotfiles/.vim /home/$USER/.vim
 git clone https://github.com/Lokaltog/vim-easymotion ~/.vim/bundle/vim-easymotion
+mkdir -p /home/$USER/.local/share/fonts
+cp /home/$USER/dotfiles/pl_fontfiles/10-powerline-symbols.conf /home/$USER/.local/share/fonts/
+mkdir -p /home/$USER/.config/fontconfig/fonts.conf
+cp /home/$USER/dotfiles/pl_fontfiles/PowerlineSymbols.otf /home/$USER/.config/fontconfig/fonts.conf/
+
+
+
